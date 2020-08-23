c37d47a747bc96088c0faa0005f8a35a21221612
sebastian duchene <sebastianduchene@sebastians-MacBook-Air-2.local>
2020-08-24 09:43:39 +1000

    checked that code runs on hcc tree

--- check_lineage_stats.R
+++ check_lineage_stats.R
@@ -1,21 +1,21 @@
 library(NELSI)
-source("../imports_analysis/get_clades.R")
+source("get_clades.R")
 
-
-
-hcc_tree <- read.nexus('NZ_genomes_V2_SC_CC.tre')#trees[[i]]
+#Highest-clade-creibility tree
 hcc_tree <- read.nexus('NZ_genomes_V3_SC_CC.tre')
-    #
+
+#Some summary data from the tree
 youngest_date <- max(as.numeric(gsub('.+[|]', '', hcc_tree$tip.label)))
-                                        #
 tips <- hcc_tree$tip.label
 node_times <- youngest_date - allnode.times(hcc_tree, reverse = F)
-                                        #
+
+#Transmission lineages and importations are those with the following regular expression tag:
 tag <- 'Locally|NA|Import_related|New_Zealand|invest'
-                                        #tag <- 'Locally|NA|Importrelated|New_Zealand|invest|Import'
-                                        #
+
+# Find monophyletic groups and singletons with the tag above
 clades <- find.monophyletic(hcc_tree, tag, include.singletons = T)
-                                        #
+
+# Get closest sister taxon, if the sister is a clade take the oldest tip
 first_sisters <- vector()
 first_sister_dates <- vector()
 for(i in 1:length(clades)){
@@ -24,15 +24,21 @@ for(i in 1:length(clades)){
     first_sisters[i] <- sister_temp[which.min(sister_dates)]
     first_sister_dates[i] <- min(sister_dates)
 }
-                                        #
+
+# Get tmrcas of clades
 clades_indexed <- lapply(clades, function(x) which(hcc_tree$tip.label %in% x))
 tmrcas <- sapply(clades_indexed,
                  function(x)
                      node_times[get.mrca(hcc_tree, x)])
+
+# Find oldest and youngest sampling times for each clade
 first_samples <- sapply(clades_indexed, function(x) min(node_times[x]))
-#
 last_samples <- sapply(clades_indexed, function(x) max(node_times[x]))
+
+# Number of samples per clade
 num_samples <- sapply(clades, function(x) length(x))
+
+#Make summary matrix
 tree_summary <- matrix(NA, length(clades), 7)
 colnames(tree_summary) <- c('tmrca', 'first_sample_date',
                             'last_sample_date', 'num_samples',
@@ -51,11 +57,7 @@ write.table(tree_summary, file = 'hcc_tree_clades_summary_V3.csv',
 
 
 
-
-
-
 ## Now process for 1000 trees from the posterior:
-
 trees <- tail(read.nexus('NZ_genomes_V3_SC_CC.trees'), 1000)
 
 credible_summary <- matrix(NA, length(trees), 3)
